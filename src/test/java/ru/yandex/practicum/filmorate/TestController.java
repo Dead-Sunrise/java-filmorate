d9ed2193f5c703bfeadb66eede05575536eef66e
DeadSunrise <7worldwot7@gmail.com>
2025-10-05 21:24:48 +0300

    Фз12 внесены правки после ревью #7

---
+++ src/test/java/ru/yandex/practicum/filmorate/TestController.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@ConditionalOnProperty(name = "test.enabled", havingValue = "true")
+public class TestController {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public TestController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostMapping("/reset")
+    public void resetDatabase() {
+        jdbcTemplate.update("DELETE FROM film_likes");
+        jdbcTemplate.update("DELETE FROM film_genre");
+        jdbcTemplate.update("DELETE FROM friends");
+        jdbcTemplate.update("DELETE FROM films");
+        jdbcTemplate.update("DELETE FROM users");
+        jdbcTemplate.update("ALTER TABLE films ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE users ALTER COLUMN id RESTART WITH 1");
+    }
+}
