2a6b5d19d1e99408378ac4cc527377771cd718c3
JeeN4MeR <ibakulev04@gmail.com>
2025-10-20 21:05:19 +0400

    Fixed checks 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
@@ -5,6 +5,8 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface FeedStorage {
+
     List<Event> getFeedByUserId(Long userId);
+
     Event addEvent(Event event);
 }
